760a686aef5202bd4563e0e79d696852ba03635c
Ian Carroll <icarroll@sesync.org>
2017-01-17 15:33:32 -0500

    specify cran mirror for install

--- funcs.R
+++ funcs.R
@@ -9,7 +9,7 @@ pkgs <- data.frame(
 for (i in 1:nrow(pkgs)) {
     if (!(pkgs$name[i] %in% installed.packages()) || 
         packageVersion(pkgs$name[i]) < pkgs$version[i]) {
-        install.packages(pkgs$name[i])
+        install.packages(pkgs$name[i], repos="http://cran.us.r-project.org")
     }
     library(pkgs$name[i], character.only = TRUE)
 }
